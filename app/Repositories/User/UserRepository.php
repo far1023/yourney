593dd1edd62efbc0dc1041bc4530c905b5fabdaa
far1023 <fuadagilr@gmail.com>
2025-03-11 10:48:19 +0700

    removing search and sort, push with complete feat

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -19,21 +19,6 @@ class UserRepository implements UserRepositoryInterface
         // dd($validatedQueryParams);
         $query = User::query();
 
-        if (isset($validatedQueryParams->search) && !empty($validatedQueryParams->search)) {
-            $searchTerm = $validatedQueryParams->search;
-            $query->where(function ($q) use ($searchTerm) {
-                $q->where('name', 'like', "%{$searchTerm}%")
-                  ->orWhere('email', 'like', "%{$searchTerm}%");
-            });
-        }
-
-        if (isset($validatedQueryParams->sort) && !empty($validatedQueryParams->sort)) {
-            $direction = $validatedQueryParams['order'] && $validatedQueryParams['order'] === 'desc' ? 'desc' : 'asc';
-            $query->orderBy($validatedQueryParams->sort, $direction);
-        } else {
-            $query->orderBy('id', 'asc');
-        }
-
         $users = $query->paginate($validatedQueryParams->perPage);
 
         return $users;
